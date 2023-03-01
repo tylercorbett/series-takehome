5bf837147af31df22c38b3cf971c1d651e6b7cee
Tyler Corbett <thetylercorbett@gmail.com>
2023-03-01 14:02:35 -0700

    clean up

--- TypeScript/app/rose-garden.ts
+++ TypeScript/app/rose-garden.ts
@@ -19,46 +19,50 @@ export class RoseGarden {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
+      const name = this.items[i].name;
+      let quality = this.items[i].quality;
+      let sellIn = this.items[i].sellIn;
+
+      if (name !== 'Aged Brie' && name !== 'Backstage passes to a TAFKAL80ETC concert') {
+        if (quality > 0) {
+          if (name !== 'Sulfuras, Hand of Ragnaros') {
+            quality = quality - 1
           }
         }
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
+        if (quality < 50) {
+          quality = quality + 1
+          if (name === 'Backstage passes to a TAFKAL80ETC concert') {
+            if (sellIn < 11) {
+              if (quality < 50) {
+                quality = quality + 1
               }
             }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
+            if (sellIn < 6) {
+              if (quality < 50) {
+                quality = quality + 1
               }
             }
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+      if (name !== 'Sulfuras, Hand of Ragnaros') {
+        sellIn = sellIn - 1;
       }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
+      if (sellIn < 0) {
+        if (name !== 'Aged Brie') {
+          if (name !== 'Backstage passes to a TAFKAL80ETC concert') {
+            if (quality > 0) {
+              if (name !== 'Sulfuras, Hand of Ragnaros') {
+                quality = quality - 1
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
+            quality = quality - quality
           }
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
+          if (quality < 50) {
+            quality = quality + 1
           }
         }
       }
--- TypeScript/test/jest/rose-garden.spec.ts
+++ TypeScript/test/jest/rose-garden.spec.ts
@@ -4,6 +4,31 @@ describe('Rose Garden', () => {
   it('should foo', () => {
     const roseGarden = new RoseGarden([new Item('foo', 0, 0)]);
     const items = roseGarden.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].name).toBe('foo');
   });
+
+  // Once the sell by date has passed, Quality degrades twice as fast
+  it('checks that the quality degrades twice as fast after sell date has past', () => {
+    const roseGarden = new RoseGarden([new Item('foo', 0, 0)]);
+    const items = roseGarden.updateQuality();
+
+    
+  });
+
+  // The Quality of an item is never negative
+
+  
+  // "Aged Brie" actually increases in Quality the older it gets
+
+
+  // The Quality of an item is never more than 50
+
+
+  // "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+  // "Sulfuras" Quality is 80 and it never alters.
+
+  // "Backstage passes"
+    // Quality increases by 2 when there are 10 days or less
+    // Quality increases by 3 when there are 5 days or less
+    // Quality drops to 0 after the concert
 });
